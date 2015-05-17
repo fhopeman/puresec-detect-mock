25d044ebbbdf4d56b5ccb5eb8b7d210068925042
Fabian Hoffmann <fabianhoffmann_@web.de>
2015-05-17 20:42:25 +0200

    use registration of puresec-microservice-js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var master = require("./master.js");
 var puresecMicroservice = require("puresec-microservice-js");
 
 var app = express();
@@ -14,9 +13,55 @@ app.get("/health", function(req, res) {
     res.send("OK");
 });
 
-app.listen(port, function() {
-    var url = puresecMicroservice.currentAddress() + ":" + port;
-    console.log("detect dummy microservice listening at '%s'", url);
 
-    master.registerAndContinouslyAlert(url, urlMaster, registrationInterval, alertInterval);
+var triggerAlarm = function(urlMaster, registrationId) {
+    console.log("\ntriggering alarm ..");
+
+    request({
+        uri: urlMaster + "/alarm/notify",
+        method: "POST",
+        form: {
+            detector_id: registrationId
+        }
+    }, function(error, _, body) {
+        if (!error) {
+            console.log("result:", body);
+        } else {
+            console.log("error during alarm notification", error);
+        }
+    });
+};
+
+var startAlertingLoop = function(registrationId) {
+    console.log("\nstarting alert loop (each %s seconds)", alertInterval);
+
+    setInterval(function() {
+        triggerAlarm(urlMaster, registrationId);
+    }, alertInterval * 1000);
+};
+
+app.listen(port, function() {
+    var urlClient = puresecMicroservice.utils().currentAddress() + ":" + port;
+    var master = puresecMicroservice.master(urlMaster);
+
+    var registerOptions = {
+        name: "Mock Handler 1",
+        description: "Mock implementation of handler",
+        type: "detector",
+        address: urlClient,
+        onSuccess: function(jsonBody) {
+            console.log("result: ", jsonBody);
+            startAlertingLoop(jsonBody.id);
+        },
+        onError: function(error) {
+            console.log("error during registration", error, "\nretry ..");
+            setTimeout(function() {
+                master.register(registerOptions);
+            }, registrationInterval * 1000);
+        }
+    };
+
+    // register
+    console.log("try to register ..");
+    master.register(registerOptions);
 });
